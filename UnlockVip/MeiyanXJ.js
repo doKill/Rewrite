42e3211086c30733fb6d48e3bb62e088ce36a281
拖地僧 <doKill@users.noreply.github.com>
2024-08-01 19:33:13 +0800

    Update MeiyanXJ.js

--- UnlockVip/MeiyanXJ.js
+++ UnlockVip/MeiyanXJ.js
@@ -1,5 +1,4 @@
-[[rewrite_local]
-
+[rewrite_local]
 ^https:\/\/api\.meiyan\.com\/vip\/user_info\.json url script-response-body https://raw.githubusercontent.com/doKill/Rewrite/master/UnlockVip/MeiyanXJ.js
 
 [mitm] 
@@ -10,26 +9,26 @@ var body = $response.body;
 var objk = JSON.parse(body);
 
 objk = {
-      "meta": {
-            "code": 0,
-            "msg": "",
-            "error": "",
-            "request_uri": "/vip/user_info",
-            "reqid": "f0b087dccf252a2cd1bac9f3bb0c17bc"
-      },
-      "response": {
-            "status": 1,
-            "agreement_status": 1,
-            "agreement_platform": 0,
-            "expire_date": "2029-10-15",
-            "expire_time": "",
-            "period_type": 0,
-            "product_type": 0,
-            "permission": [],
-            "type": 0,
-            "ad_vip_type": 0,
-            "discount_status": 0
-      }
+    "meta": {
+        "code": 0,
+        "msg": "",
+        "error": "",
+        "request_uri": "/vip/user_info",
+        "reqid": "f0b087dccf252a2cd1bac9f3bb0c17bc"
+    },
+    "response": {
+        "status": 1,
+        "agreement_status": 1,
+        "agreement_platform": 0,
+        "expire_date": "2029-10-15",
+        "expire_time": "",
+        "period_type": 0,
+        "product_type": 0,
+        "permission": [],
+        "type": 0,
+        "ad_vip_type": 0,
+        "discount_status": 0
+    }
 }
 
 body = JSON.stringify(objk);
