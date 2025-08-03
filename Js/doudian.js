ab3cd28ef97bdbf46f2b2653fd5f3b9150bebd1c
huang <funccn@gmail.com>
2025-08-03 23:06:41 +0800

    test

--- Js/doudian.js
+++ Js/doudian.js
@@ -23,20 +23,30 @@ function modifyResponse (body) {
 
             const { province: { name: provinceName } = {}, city: { name: cityName } = {} } = productItem.receiver_info?.post_addr || {};
 
-            productItem.policy_info = [{
-                "ins_policy_no": "10202508012230091012319241166",
-                "policy_type": "returnfreight2020v1",
-                "policy_type_text": "地址",
-                "status": 1,
-                "status_desc": "待生效",
-                "detail_url": "https://fxg.baohuaxia.com/h5/trade/insurance?order_id=6944846915338835619\u0026ins_policy_no=10202508012230091012319241166\u0026title=%E8%BF%90%E8%B4%B9%E9%99%A9%E8%AF%A6%E6%83%85\u0026title_color=ffffff\u0026nav_bar_color=1a66ff\u0026status_bar_color=1a66ff\u0026status_font_dark=0\u0026hide_more=0\u0026back_btn_dark=0",
-                "policy_detail_text": `${provinceName}${cityName}`
-            }]
+            productItem.policy_info = [
+                {
+                    "ins_policy_no": "10202508012230091012319241166",
+                    "policy_type": "returnfreight2020v1",
+                    "policy_type_text": "地址",
+                    "status": 1,
+                    "status_desc": "待生效",
+                    "detail_url": "",
+                    "policy_detail_text": `111${provinceName}${cityName}`
+                }
+            ];
 
-            if (productItem.privilege_info_list != null && productItem.privilege_info_list.length > 0) {
-                productItem.privilege_info_list[0].privilege_name = "地址";
-                productItem.privilege_info_list[0].status_desc = `${provinceName || ''}${cityName || ''}`;
-            }
+
+            productItem.privilege_info_list = [
+                {
+                    "privilege_code": "returnfreight2020v1",
+                    "privilege_name": "地址",
+                    "status": 1,
+                    "status_desc": `222${provinceName}${cityName}`,
+                    "detail_url": "",
+                    "ins_policy_no": "10202508020002391456406570203"
+                }
+            ];
+
 
 
             if (!productItem.tags || !Array.isArray(productItem.tags)) {
